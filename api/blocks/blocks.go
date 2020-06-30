02608562efd2fb72e1ef1aa0429bb6ecffe3d05e
yang <yyu@dfinlab.com>
2020-06-30 12:09:31 -0700

    finalize the function getKBlockByEpoch

--- api/blocks/blocks.go
+++ api/blocks/blocks.go
@@ -6,6 +6,7 @@
 package blocks
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -124,6 +125,95 @@ func (b *Blocks) getBlock(revision interface{}) (*block.Block, error) {
 		return b.chain.LeafBlock(), nil
 	default:
 		return b.chain.BestBlock(), nil
+	}
+}
+
+func (b *Blocks) getKBlockByEpoch(epoch uint64) (*block.Block, error) {
+	best := b.chain.BestBlock()
+	curEpoch := best.GetBlockEpoch()
+	if epoch > curEpoch {
+		fmt.Println("requested epoch is too new", "epoch:", epoch)
+		return nil, errors.New("requested epoch is too new")
+	}
+
+	//fmt.Println("getKBlockByEpoch", "epoch", epoch, "curEpoch", curEpoch)
+	delta := uint64(best.Header().Number()) / curEpoch
+
+	var blk *block.Block
+	var ht, ep uint64
+	var err error
+	if curEpoch-epoch <= 5 {
+		blk = best
+		ht = uint64(best.Header().Number())
+		ep = curEpoch
+	} else {
+		ht := delta * (epoch + 4)
+		blk, err = b.chain.GetTrunkBlock(uint32(ht))
+		if err != nil {
+			fmt.Println("get the kblock failed", "epoch", epoch, "error", err)
+			return nil, err
+		}
+
+		ep = blk.GetBlockEpoch()
+		for ep < epoch {
+			ht = ht + (4 * delta)
+			if ht >= uint64(best.Header().Number()) {
+				ht = uint64(best.Header().Number())
+				blk = best
+				ep = curEpoch
+				break
+			}
+
+			blk, err = b.chain.GetTrunkBlock(uint32(ht) + uint32(5*delta))
+			if err != nil {
+				fmt.Println("get the kblock failed", "epoch", epoch, "error", err)
+				return nil, err
+			}
+			ep = blk.GetBlockEpoch()
+			//fmt.Println("... height:", blk.Header().Number(), "epoch", ep)
+		}
+	}
+
+	// find out the close enough search point
+	// fmt.Println("start to search kblock", "height:", blk.Header().Number(), "epoch", ep, "target epoch", epoch)
+	for ep > epoch {
+		blk, err = b.chain.GetTrunkBlock(blk.Header().LastKBlockHeight())
+		if err != nil {
+			fmt.Println("get the TrunkBlock failed", "epoch", epoch, "error", err)
+			return nil, err
+		}
+		ep = blk.GetBlockEpoch()
+		//fmt.Println("...searching height:", blk.Header().Number(), "epoch", ep)
+	}
+
+	//fmt.Println("get the kblock", "height:", blk.Header().Number(), "epoch", ep)
+	if ep == epoch {
+		return blk, nil
+	}
+
+	// now ep < epoch for some reason.
+	ht = uint64(blk.Header().Number() + 1)
+	count := 0
+	for {
+		blk, err = b.chain.GetTrunkBlock(uint32(ht))
+		if err != nil {
+			fmt.Println("get the TrunkBlock failed", "epoch", epoch, "error", err)
+			return nil, err
+		}
+		ep = blk.GetBlockEpoch()
+
+		if ep == epoch && blk.Header().BlockType() == block.BLOCK_TYPE_K_BLOCK {
+			return blk, nil // find it !!!
+		}
+		if ep > epoch {
+			return nil, errors.New("can not find the kblock")
+		}
+
+		count++
+		if count >= 2000 {
+			return nil, errors.New("can not find the kblock")
+		}
+		//fmt.Println("...final search. height:", ht, "epoch", ep)
 	}
 }
 
